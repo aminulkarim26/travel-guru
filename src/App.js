063c3bc3bdbfbd325b09bc69aca3acd6715819cc
aminulkarim26 <aminul.mkt@gmail.com>
2020-09-25 21:42:58 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import './App.css';
-import Home from './Components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Booking from './components/Booking/Booking';
+import travel from './Fake/travel';
+import BookingDetails from './components/BookingDetails/BookingDetails';
+import LogIn from './components/LogIn/LogIn';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Destination from './components/Destination/Destination';
+import Blog from './components/Blog/Blog';
+import Contact from './components/Contact/Contact';
+import NotFound from './components/NotFound/NotFound';
 
+
+
+export const UserContext = createContext()
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState(travel)
   return (
-    
-    <div>
-      <Home></Home>
-    </div>  
-    
+    <div className='app'>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+      <Header></Header>
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/destination">
+            <Destination />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+          <Route path="contact">
+            <Contact />
+          </Route>
+          <Route path="/booking/:bookingId">
+            <Booking />
+          </Route>
+          <PrivateRoute path="/about/:id">
+            <BookingDetails />
+          </PrivateRoute>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+    </div>
+
   );
 }
 
---
+++ src/components/LogIn/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBhd0VMgkivAZW6mm-27G_kugMWPDL1Gcw",
+  authDomain: "travel-guru-a7afd.firebaseapp.com",
+  databaseURL: "https://travel-guru-a7afd.firebaseio.com",
+  projectId: "travel-guru-a7afd",
+  storageBucket: "travel-guru-a7afd.appspot.com",
+  messagingSenderId: "989205266445",
+  appId: "1:989205266445:web:38ab5e3238e0e155035544"
+  };
+
+export default firebaseConfig;
